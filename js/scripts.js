1b979209b1d7fe30b3b189f306760cecf4256504
mary-wan <mary.njenga@student.moringaschool.com>
2021-09-23 08:43:54 +0300

    add redirect after successful login

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
 $(document).ready(function () {
 
-    var name,email,password;
-   
+    var email, password, regName, regPassword, regEmail, passwordConfirm;
+
     $(".rent").click(function () {
         location.replace("our-fleet.html");
 
     });
     $(".Login").click(function (e) {
         e.preventDefault();
-        name = $("input#username").val();
         email = $("input#user-email").val();
         password = $("input#user-password").val();
-      
-        if(name=="" ||name == null ||email=="" ||email == null ||password=="" ||password == null ){
-            alert("Please fill in all details")
-        }else{
-            location.replace("our-fleet.html.html");
+
+        if (email == "" || email == null || password == "" || password == null) {
+            $(".display").show().text("Please fill in all details")
+        } else {
+            $(".display").hide();
+            location.replace("our-fleet.html");
         }
 
     });
-    $(".Signup").click(function () {
-        location.replace("our-fleet.html.html");
-        // alert("ghj")
+    $(".Signup").click(function (e) {
+        e.preventDefault()
+        regName = $("input#UserName").val();
+        regEmail = $("input#Email").val();
+        regPassword = $("input#password").val();
+        passwordConfirm = $("input#confirm-password").val();
 
+        if (regName == "" || regName == null || regEmail == "" || regEmail == null || regPassword == "" ||
+            regPassword == null || passwordConfirm == "" || passwordConfirm == null) {
+            $(".display").show().text("Please fill in all details")
+        } else if (regPassword != passwordConfirm) {
+            $(".display").show().text("Passwords do not match")
+            $("input#password").val("");
+            $("input#confirm-password").val("");
+        } else {
+            $(".display").hide()
+            location.replace("our-fleet.html");
+        }
     });
 });
